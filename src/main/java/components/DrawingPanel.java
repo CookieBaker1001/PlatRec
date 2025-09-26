b68a78812639cf17c7ad667099c0de54bd05b50f
CookieBaker1001 <simon.springer1001@gmail.com>
2025-09-26 17:03:23 +0200

    Can now zoom and pan around.

--- src/main/java/components/DrawingPanel.java
+++ src/main/java/components/DrawingPanel.java
@@ -12,18 +12,30 @@ import static util.Constants.*;
 
 public class DrawingPanel extends JPanel implements MouseListener, MouseMotionListener {
 
-    ArrayList<CanvasObject> canvasObjects = new ArrayList<>();
+    private final ArrayList<CanvasObject> canvasObjects = new ArrayList<>();
+    private final Point originOfClick = new Point(-1, -1);
+
+    private double zoom = 1.0;
+    private double offsetX = 0;
+    private double offsetY = 0;
+    private boolean isPanning = false;
+    private Point panStartPoint = new Point(0, 0);
 
     public DrawingPanel() {
         setBackground(drawingPanelColor);
         addMouseListener(this);
         addMouseMotionListener(this);
+        addMouseWheelListenerHelper();
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D) g.create();
+
+        g2.translate(offsetX, offsetY);
+        g2.scale(zoom, zoom);
+        drawCanvasBoard(g2);
 
         g2.setColor(Color.red);
         for (CanvasObject obj : canvasObjects) {
@@ -34,27 +46,66 @@ public class DrawingPanel extends JPanel implements MouseListener, MouseMotionLi
         g2.dispose();
     }
 
-    @Override
-    public void mouseClicked(MouseEvent e) {}
+    private void drawCanvasBoard(Graphics2D g2) {
+        g2.setColor(drawingCanvasColor);
+        g2.fillRect(0, 0, getWidth(), getHeight());
+    }
 
-    int x, y;
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
 
     @Override
     public void mousePressed(MouseEvent e) {
-        x = e.getX();
-        y = e.getY();
+        if (SwingUtilities.isMiddleMouseButton(e)) {
+            isPanning = true;
+            panStartPoint = e.getPoint();
+        } else if (SwingUtilities.isLeftMouseButton(e)) {
+            Point p = screenToCanvas(e.getPoint());
+            originOfClick.x = p.x;
+            originOfClick.y = p.y;
+        }
     }
 
     @Override
     public void mouseReleased(MouseEvent e) {
-        CanvasObject canvasObject = new CanvasObject();
-        canvasObject.x = Math.min(x, e.getX());
-        canvasObject.y = Math.min(y, e.getY());
-        canvasObject.width = Math.abs(e.getX() - x);
-        canvasObject.height = Math.abs(e.getY() - y);
-        canvasObjects.add(canvasObject);
-        x = -1;
-        y = -1;
+        if (SwingUtilities.isMiddleMouseButton(e)) {
+            isPanning = false;
+            panStartPoint = null;
+        } else if (SwingUtilities.isLeftMouseButton(e)) {
+            Point p = screenToCanvas(e.getPoint());
+            CanvasObject canvasObject = new CanvasObject();
+            canvasObject.x = Math.min(originOfClick.x, p.x);
+            canvasObject.y = Math.min(originOfClick.y, p.y);
+            canvasObject.width = Math.abs(p.x - originOfClick.x);
+            canvasObject.height = Math.abs(p.y - originOfClick.y);
+            canvasObjects.add(canvasObject);
+            originOfClick.x = -1;
+            originOfClick.y = -1;
+        }
+
+    }
+
+    public Point screenToCanvas(Point p) {
+        int canvasX = (int) ((p.x - offsetX) / zoom);
+        int canvasY = (int) ((p.y - offsetY) / zoom);
+        return new Point(canvasX, canvasY);
+    }
+
+    private void addMouseWheelListenerHelper() {
+        addMouseWheelListener(e -> {
+            Point mousePoint = e.getPoint();
+            double zoomCenterX = (mousePoint.x - offsetX) / zoom;
+            double zoomCenterY = (mousePoint.y - offsetY) / zoom;
+            double zoomFactor = e.getWheelRotation() < 0 ? 1.1 : 0.9;
+            zoom *= zoomFactor;
+            zoom = Math.max(0.1, Math.min(zoom, 10.0));
+            offsetX = mousePoint.x - zoomCenterX * zoom;
+            offsetY = mousePoint.y - zoomCenterY * zoom;
+            repaint();
+            requestFocusInWindow();
+        });
     }
 
     @Override
@@ -64,7 +115,15 @@ public class DrawingPanel extends JPanel implements MouseListener, MouseMotionLi
     public void mouseExited(MouseEvent e) {}
 
     @Override
-    public void mouseDragged(MouseEvent e) {}
+    public void mouseDragged(MouseEvent e) {
+        if (isPanning) {
+            int dx = e.getX() - panStartPoint.x;
+            int dy = e.getY() - panStartPoint.y;
+            offsetX += dx;
+            offsetY += dy;
+            panStartPoint = e.getPoint();
+        }
+    }
 
     @Override
     public void mouseMoved(MouseEvent e) {}
--- src/main/java/util/Constants.java
+++ src/main/java/util/Constants.java
@@ -5,7 +5,8 @@ import java.awt.*;
 public final class Constants {
 
     public final static Color mainBGcolor = new Color(150, 150, 150);
-    public final static Color drawingPanelColor = new Color(0, 200, 200);
+    public final static Color drawingPanelColor = new Color(200, 200, 200);
+    public final static Color drawingCanvasColor = new Color(255, 255, 255);
     public final static Color sidePanelColor = new Color(100, 100, 100);
     public final static int sidePanelWidth = 150;
 
